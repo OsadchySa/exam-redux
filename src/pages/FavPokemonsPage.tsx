c7d17ffe2f9ea88fc5eb62e41cd1de59ae97837e
OsadchySa <osadchyalexandr0790@gmail.com>
2024-08-24 15:32:53 +0300

    24.08 15:33

--- src/pages/FavPokemonsPage.tsx
+++ src/pages/FavPokemonsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const FavPokemonsPage: React.FC = () => {
     const [favorites, setFavorites] = React.useState<any[]>([])
