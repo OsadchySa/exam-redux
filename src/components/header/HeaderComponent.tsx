d27e574b1e9de8e371d3a69f319ff4da343a38f7
OsadchySa <osadchyalexandr0790@gmail.com>
2024-08-24 15:32:22 +0300

    24.08 15:32

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -7,6 +7,18 @@ const Header = () => {
             <ul className={'headerUl'}>
                 <Link className={'headLink'} to={'/pokemons'}>TO POKEMONS PAGE</Link>
                 <Link className={'headLink'} to={'/favorites'}>TO FAVORITE POKEMON PAGE</Link>
+                <form>
+                    <input/>
+                    <button>Search Pokemon</button>
+                </form>
+                <form>
+                    <input/>
+                    <button>Search by type</button>
+                </form>
+                <form>
+                    <input/>
+                    <button>Search by ability</button>
+                </form>
                 <hr/>
             </ul>
         </div>
--- src/pages/FavPokemonsPage.tsx
+++ src/pages/FavPokemonsPage.tsx
@@ -2,30 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const FavPokemonsPage: React.FC = () => {
-    const [favorites, setFavorites] = React.useState<any[]>([]);
+    const [favorites, setFavorites] = React.useState<any[]>([])
 
     React.useEffect(() => {
-        const savedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
-        setFavorites(savedFavorites);
-    }, []);
+        const savedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]')
+        setFavorites(savedFavorites)
+    }, [])
 
     return (
         <div>
-            <h1>Favorite Pokémons</h1>
-            {favorites.length === 0 ? (
-                <div>No favorite pokémons found.</div>
-            ) : (
-                <div className="allPokDiv">
-                    {favorites.map((pokemon: any) => (
-                        <Link to={'/pokemons/' + pokemon.id} key={pokemon.id}>
-                            <div className={'miniPokDiv'}>
-                                <h3>{pokemon.name}</h3>
-                                <img src={pokemon.image} alt={pokemon.name} />
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            )}
+            <h1>Favorite Pokemons</h1>
+            <div className="allPokDiv">
+                {favorites.map((pokemon: any) => (
+                    <Link to={'/pokemons/' + pokemon.id} key={pokemon.id}>
+                        <div className={'miniPokDiv'}>
+                            <h3>{pokemon.name}</h3>
+                            <img src={pokemon.image} alt={pokemon.name} />
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
